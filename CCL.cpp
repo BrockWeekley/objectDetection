b8fda67e0f63f27c73a512b8c27afdb2285485d6
BrockWeekley <48456042+BrockWeekley@users.noreply.github.com>
2021-09-30 22:13:36 -0500

    Significant progress - first pass completed, colorization completed, area filtering done

--- CCL.cpp
+++ CCL.cpp
@@ -36,9 +36,10 @@ Color random_color() {
 
 int main(int argc, char** argv )
 {
-    if ( argc != 3 )
+    if ( argc < 3 || argc > 6 )
     {
         printf("usage: ./CCL [-invert] <input_image> <output_image> \n");
+        printf("or: ./CCL [-invert] -size <number> <input_image> <output_image>");
         return -1;
     }
     srand(time(0));
@@ -49,7 +50,7 @@ int main(int argc, char** argv )
         // Invert flag used
         invert = true;
     }
-    
+    cv::Mat image;
     char* arguments [argc];
     arguments[0] = argv[0];
     if (invert) {
@@ -58,14 +59,18 @@ int main(int argc, char** argv )
             arguments[i] = argv[i + 1];
             
         }
+        image = cv::imread(arguments[argc - 3], 0);
+        printf("\n\n%s\n\n", arguments[argc - 3]);
     } else {
         int i;
         for (i = 1; i < argc; i++) {
             arguments[i] = argv[i];
         }
+        image = cv::imread(arguments[argc - 2], 0);
+        printf("\n\n%s\n\n", arguments[argc - 2]);
     }
-    
-    cv::Mat image = cv::imread(arguments[1], 0);
+
+
     if (image.channels() > 2) {
         cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
     }
@@ -83,28 +88,198 @@ int main(int argc, char** argv )
     cv::merge(planes, 3, newImage);
     height = newImage.size[0];
     width = newImage.size[1];
-    
-    std::vector<int> parent;
+
+    printf("Height of Image: %d, Width of Image: %d", height, width);
     
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
-//            cv::Vec3b color = newImage.at<cv::Vec3b>(row, col);
-//            Color randomColor = random_color();
-//            printf("\n Blue: %d, Green: %d, Red: %d", randomColor.blue, randomColor.green, randomColor.red);
-//            // B
-//            color[0] = randomColor.blue;
-//            // G
-//            color[1] = randomColor.green;
-//            // R
-//            color[2] = randomColor.red;
-//            newImage.at<cv::Vec3b>(row, col) = color;
+            if (invert) {
+                if (image.at<uchar>(row, col) == 255) {
+                    image.at<uchar>(row, col) = 0;
+                    cv::Vec3b pixel = newImage.at<cv::Vec3b>(row, col);
+                    pixel[0] = 0;
+                    pixel[1] = 0;
+                    pixel[2] = 0;
+                    newImage.at<cv::Vec3b>(row, col) = pixel;
+                } else {
+                    image.at<uchar>(row, col) = 255;
+                    cv::Vec3b pixel = newImage.at<cv::Vec3b>(row, col);
+                    pixel[0] = 255;
+                    pixel[1] = 255;
+                    pixel[2] = 255;
+                    newImage.at<cv::Vec3b>(row, col) = pixel;
+                }
+            }
         }
+    }
+    
+    // Initialize all labels to 0
+    int labels [height][width];
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+            labels[row][col] = 0;
+        }
+    }
+
+    std::vector<int> parent;
+    int label = 1;
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+            // e is the current pixel
+            // 8 connected structure is:
+            // a b c
+            // d e
+            int e = image.at<uchar>(row, col);
+            if (e == 255) {
+                int d = 0;
+                int a = 0;
+                int b = 0;
+                int c = 0;
+                if (col - 1 >= 0 && image.at<uchar>(row, col - 1) == 255) {
+                    d = 1;
+                }
+                if (row - 1 >= 0 && col - 1 >= 0 && image.at<uchar>(row - 1, col - 1) == 255) {
+                    a = 1;
+                }
+                if (row - 1 >= 0 && image.at<uchar>(row - 1, col) == 255) {
+                    b = 1;
+                }
+                if (row - 1 >= 0 && col + 1 <= width && image.at<uchar>(row - 1, col + 1) == 255) {
+                    c = 1;
+                }
+                
+                if (d == 1) {
+
+                    if (a == 1) {
+                        // 1 0 0
+                        // 1 a
+                        labels[row][col] = labels[row - 1][col - 1];
+                    }
+                    if (b == 1) {
+                        // 0 1 0
+                        // 1 b
+                        labels[row][col] = labels[row - 1][col];
+                    }
+                    if (c == 1) {
+                        // 0 0 1
+                        // 1 c
+                        // Union c and d
+                        labels[row][col] = labels[row - 1][col + 1];
+                    }
+                    // 0 0 0
+                    // 1 d
+                    labels[row][col] = labels[row][col - 1];
+                } else if (a == 1) {
+                    
+                    if (b == 1) {
+                        // 1 1 0
+                        // 0 b
+                        labels[row][col] = labels[row - 1][col];
+                    }
+                    if (c == 1) {
+                        // 1 0 1
+                        // 0 a
+                        // Union a and c
+                        labels[row][col] = labels[row - 1][col - 1];
+                    }
+                    // 1 0 0
+                    // 0 a
+                    labels[row][col] = labels[row - 1][col - 1];
+                } else if (b == 1) {
+                    
+                    if (c == 1) {
+                        // 0 1 1
+                        // 0 b
+                        labels[row][col] = labels[row - 1][col];
+                    }
+                    // 0 1 0
+                    // 0 b
+                    labels[row][col] = labels[row - 1][col];
+                } else if (c == 1) {
+                    // 0 0 1
+                    // 0 c
+                    labels[row][col] = labels[row - 1][col + 1];
+                } else {
+                    labels[row][col] = label;
+                    label++;
+                }
+            }
+//            if (image.at<uchar>(row, col) == 255) {
+//                cv::Vec3b color = newImage.at<cv::Vec3b>(row, col);
+//                Color randomColor = random_color();
+//                printf("\n Blue: %d, Green: %d, Red: %d", randomColor.blue, randomColor.green, randomColor.red);
+//                // B
+//                color[0] = randomColor.blue;
+//                // G
+//                color[1] = randomColor.green;
+//                // R
+//                color[2] = randomColor.red;
+//                newImage.at<cv::Vec3b>(row, col) = color;
+//            }
+        }
+    }
+    Color labelColor [label];
+    labelColor[0] = {0, 0, 0};
+    for (int currentLabel = 1; currentLabel <= label; currentLabel++) {
+        labelColor[currentLabel] = random_color();
+    }
+    
+    int objectAreas [label];
+    objectAreas[0] = 0;
+    for (int currentLabel = 1; currentLabel <= label; currentLabel++) {
+        objectAreas[currentLabel] = 0;
+    }
+    // Loop through labels array to apply colors
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+            for (int currentLabel = 1; currentLabel <= label; currentLabel++) {
+                if (labels[row][col] == currentLabel) {
+                    cv::Vec3b color = newImage.at<cv::Vec3b>(row, col);
+                    Color randomColor = labelColor[currentLabel];
+                    // B
+                    color[0] = randomColor.blue;
+                    // G
+                    color[1] = randomColor.green;
+                    // R
+                    color[2] = randomColor.red;
+                    newImage.at<cv::Vec3b>(row, col) = color;
+                    
+                    objectAreas[currentLabel]++;
+                }
+            }
+        }
+    }
+    
+    for (int currentLabel = 1; currentLabel <= label; currentLabel++) {
+        printf("\nObject Area: %d", objectAreas[currentLabel]);
     }
     
     cv::imshow("Display", newImage);
     cv::waitKey(0);
     
-    if (argc > 5) {
+    if (argc > 4) {
         // Also do part C
+        int maxArea = atoi(arguments[2]);
+        printf("\n Max area: %d \n", maxArea);
+        for (int currentLabel = 1; currentLabel <= label; currentLabel++) {
+            if (objectAreas[currentLabel] < maxArea) {
+                for (int row = 0; row < height; row++) {
+                    for (int col = 0; col < width; col++) {
+                        if (labels[row][col] == currentLabel) {
+                            cv::Vec3b color = newImage.at<cv::Vec3b>(row, col);
+                            // B
+                            color[0] = 0;
+                            // G
+                            color[1] = 0;
+                            // R
+                            color[2] = 0;
+                            newImage.at<cv::Vec3b>(row, col) = color;
+                        }
+                    }
+                }
+            }
+        }
+        cv::imshow("Display", newImage);
+        cv::waitKey(0);
     }
 }
